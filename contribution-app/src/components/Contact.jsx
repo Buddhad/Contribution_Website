a5f9ed636ce3f7d47474da9e09c2b07b1bc14cf1
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-10-02 00:21:12 +0530

    Added Page and routing

--- contribution-app/src/components/Contact.jsx
+++ contribution-app/src/components/Contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
           </div>
           <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
             <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact Us</h2>
-            <p class="leading-relaxed mb-5 text-gray-600">Send us your email and query and we will contact you </p>
+            <p class="leading-relaxed mb-5 text-gray-600">Send us your email and message and we will contact you </p>
             <div class="relative mb-4">
               <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
               <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
