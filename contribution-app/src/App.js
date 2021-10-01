724b3c558dc5c58df49eb531cb44fa0455bb7873
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-10-02 00:18:55 +0530

    Added Contact US page and Routing

--- contribution-app/src/App.js
+++ contribution-app/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
+import { Route } from "react-router";
+import Contact from "./components/Contact";
 function App() {
   return <div>
-  <Navbar /> 
-
-  <Content/></div>
-  
+    <Navbar />
+    <Route path='/' exact>
+      <Content />
+    </Route>
+    <Route path='/about'>
+      <Contact />
+    </Route>
+  </div>
 }
 
 export default App;
