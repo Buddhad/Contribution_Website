1252331efc4d8ec81bdbb7854e883153c603d572
Divash Gupta <guptadivash70@gmail.com>
2021-10-02 19:02:02 +0530

    Added Hamburger

--- contribution-app/src/components/Footer.jsx
+++ contribution-app/src/components/Footer.jsx
@@ -4,8 +4,7 @@ const Footer = () => {
 	return (
 		<div>
 			<footer
-				style={{ background: "#3d4451", color: "#f9fafb" }}
-				className='items-center p-4 footer '
+				className='items-center p-4 footer bg-neutral text-neutral-content border-t-2 border-gray-300'
 			>
 				<div className='items-center grid-flow-col'>
 					<svg
@@ -23,7 +22,7 @@ const Footer = () => {
 				</div>
 				<div className='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
 					<a
-					href="https://twitter.com/Buddhadeb01" target="_blank"
+					href="https://twitter.com/Buddhadeb01" target="_blank" rel="noreferrer"
 					>
 						<svg
 							xmlns='http://www.w3.org/2000/svg'
@@ -35,7 +34,7 @@ const Footer = () => {
 							<path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z'></path>
 						</svg>
 					</a>
-					<a href='https://www.youtube.com/c/GitHub' target="_blank">
+					<a href='https://www.youtube.com/c/GitHub' target="_blank" rel="noreferrer">
 						<svg
 							xmlns='http://www.w3.org/2000/svg'
 							width='24'
@@ -49,7 +48,8 @@ const Footer = () => {
 					<a
 						href='https://github.com/Buddhad/Contribution_Website'
 						target='_blank'
-						class=' text-white text-2xl'
+						class='text-2xl'
+						rel="noreferrer"
 					>
 						<FaGithub />
 					</a>
--- contribution-app/src/components/Navbar.jsx
+++ contribution-app/src/components/Navbar.jsx
@@ -1,68 +1,147 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React from "react";
+import React, { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import "../components/Navbar.css";
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
-  return (
-    <header className="text-gray-400 bg-green-300 text-black body-font">
-      <div className="w-full flex flex-wrap p-3 flex-col md:flex-row items-center">
-        <div className="flex title-font font-medium items-center  text-white mb-4 md:mb-0">
-          <Link to='/'>
-          <span className="ml-3 cursor-default text-2xl text-black">
-            {"HACKTOBERFEST 2K21"}
-          </span>
-          </Link>
-        </div>
+	const [hamburger, setHamburger] = useState(false);
+	return (
+		// <header className=' bg-gray-300 text-black body-font'>
+		// 	<div className='w-full flex flex-wrap p-3 flex-col md:flex-row items-center'>
+		// 		<div className='flex title-font font-medium items-center text-white mb-4 md:mb-0'>
+		// 			<Link to='/'>
+		// 				<img
+		// 					src='/hacktoberfest.svg'
+		// 					alt=''
+		// 					width='50px'
+		// 					height='50px'
+		// 				/>
+		// 				{/* <span className="ml-3 cursor-default text-2xl text-black">
+		//         {"HACKTOBERFEST 2K21"}
+		//       </span> */}
+		// 			</Link>
+		// 		</div>
 
-        <div class="bg-white shadow p-4 flex search_bar">
-          <span class="w-auto flex justify-end items-center text-gray-500 p-2">
-            <i class="material-icons text-3xl"></i>
-          </span>
-          <input class="w-full rounded p-2" type="text" placeholder="Hacktoberfest" />
-          <button class="bg-red-400 hover:bg-red-300 rounded text-white p-2 pl-4 pr-4">
-            <p class="font-semibold text-xs">Search</p>
-          </button>
-        </div>
+		// 		<div className='bg-white shadow p-4 flex search_bar'>
+		// 			<span className='w-auto flex justify-end items-center text-gray-500 p-2'>
+		// 				<i className='material-icons text-3xl'></i>
+		// 			</span>
+		// 			<input
+		// 				className='w-full rounded p-2'
+		// 				type='text'
+		// 				placeholder='Hacktoberfest'
+		// 			/>
+		// 			<button className='bg-red-400 hover:bg-red-300 rounded text-white p-2 pl-4 pr-4'>
+		// 				<p className='font-semibold text-xs'>Search</p>
+		// 			</button>
+		// 		</div>
+		// 		<nav className='md:ml-auto flex flex-wrap justify-center items-center text-lg '>
+		// 			<a
+		// 				exact
+		// 				activeClassName='active_link'
+		// 				target='_blank'
+		// 				href='https://hacktoberfest.digitalocean.com/'
+		// 				className='px-4 py-2 text-black'
+		// 			>
+		// 				OFFICIAL WEBSITE
+		// 			</a>
+		// 			<a
+		// 				exact
+		// 				activeClassName='active_link'
+		// 				target='_blank'
+		// 				href='https://hacktoberfest.digitalocean.com/profile'
+		// 				className=' px-4 py-2 text-black'
+		// 			>
+		// 				DASHBOARD
+		// 			</a>
+		// 			<Link to='/about' className='text-black'>
+		// 				CONTACT US
+		// 			</Link>
+		// 			<a
+		// 				href='https://github.com/Buddhad/Contribution_Website'
+		// 				target='_blank'
+		// 				className='px-4 py-2 text-black text-2xl'
+		// 			>
+		// 				<FaGithub />
+		// 			</a>
+		// 		</nav>
+		// 	</div>
+		// </header>
 
+		<div className='navbar mb-2 shadow-lg bg-neutral text-neutral-content relative'>
+			<div className='flex-1 px-2 mx-2'>
+				<Link to='/'>
+					<img
+						src='/hacktoberfest.svg'
+						alt=''
+						width='50px'
+						height='50px'
+					/>
+				</Link>
+			</div>
 
+			<div>
+				<div
+					className={hamburger ? "hidden" : "hamburger text-2xl"}
+					onClick={() => {
+						setHamburger(true);
+					}}
+				>
+					<GiHamburgerMenu />
+				</div>
+				<div
+					className={hamburger ? "text-2xl close" : " hidden"}
+					onClick={() => {
+						setHamburger(false);
+					}}
+				>
+					<IoMdClose />
+				</div>
+			</div>
 
-        <nav className="md:ml-auto flex flex-wrap justify-center items-center text-lg ">
-          <a
-            exact
-            activeClassName="active_link"
-            target="_blank"
-            href="https://hacktoberfest.digitalocean.com/"
-            className="px-4 py-2 text-black"
-          >
-            OFFICIAL WEBSITE
-          </a>
-          <a
-            exact
-            activeClassName="active_link"
-            target="_blank"
-            href="https://hacktoberfest.digitalocean.com/profile"
-            className=" px-4 py-2 text-black"
-          >
-            DASHBOARD
-          </a>
-          <Link to="/about" className="text-black">
-            CONTACT US
-          </Link>
-          <a
-            href="https://github.com/Buddhad/Contribution_Website"
-            target="_blank"
-            className="px-4 py-2 text-black text-2xl"
-          >
-            <FaGithub />
-          </a>
-         
-        </nav>
-      </div>
-    </header>
-  );
+			<div
+				className={
+					hamburger
+						? "block absolute -bottom-24 bg-neutral left-0 w-full "
+						: "flex-none hidden px-2 mx-2 lg:flex responsive-nav"
+				}
+			>
+				<div className={hamburger ? "flex flex-col" : "flex items-stretch"}>
+					<a
+						exact
+						activeClassName='active_link'
+						target='_blank'
+						href='https://hacktoberfest.digitalocean.com/'
+						className=' btn btn-ghost btn-sm rounded-btn'
+					>
+						OFFICIAL WEBSITE
+					</a>
+					<a
+						exact
+						activeClassName='active_link'
+						target='_blank'
+						href='https://hacktoberfest.digitalocean.com/profile'
+						className='btn btn-ghost btn-sm rounded-btn'
+					>
+						DASHBOARD
+					</a>
+					<a className='btn btn-ghost btn-sm rounded-btn'>
+						<Link to='/about'>CONTACT US</Link>
+					</a>
+				</div>
+			</div>
+			<a
+				href='https://github.com/Buddhad/Contribution_Website'
+				target='_blank'
+				className=' px-4 py-1 btn btn-ghost btn-sm rounded-btn text-2xl'
+			>
+				<FaGithub />
+			</a>
+		</div>
+	);
 };
 
 export default Navbar;
