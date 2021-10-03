12e26b5f30282a8ccbb7815f072289534025e31d
vedant_jain03 <vedantjainben10@gmail.com>
2021-10-03 16:28:43 +0530

    fix: updated

--- contribution-app/src/components/Content.jsx
+++ contribution-app/src/components/Content.jsx
@@ -5,7 +5,7 @@ import Profilecard from "./Profilecard";
 const Content=()=>{
     return(<>
         <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-            <img style={{width:"55%",height:"50%"}} className="p-5" src={logo} alt="logo"/>
+            <img style={{width:"55%",height:"70vh"}} className="p-5" src={logo} alt="logo"/>
         </div>
     <Profilecard/>
         </>
