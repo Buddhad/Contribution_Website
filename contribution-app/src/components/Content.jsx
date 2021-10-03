0170940e5b4492c93575f652c3ec839ef227a812
Vedant Jain <76901313+vedant-jain03@users.noreply.github.com>
2021-10-03 16:35:29 +0530

    Update Content.jsx

--- contribution-app/src/components/Content.jsx
+++ contribution-app/src/components/Content.jsx
@@ -5,7 +5,6 @@ import Profilecard from "./Profilecard";
 const Content=()=>{
     return(<>
         <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-            <img style={{width:"55%",height:"70vh"}} className="p-5" src={logo} alt="logo"/>
             <img style={{ width: "55%", marginTop: 6 + "rem",height:"50%"}} className="p-5" src={logo} alt="logo"/>
         </div>
     <Profilecard/>
