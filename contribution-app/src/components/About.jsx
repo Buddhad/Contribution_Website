634660304dfd938e087b1c8dadde418f502f7fbd
meixin <meixinchoy@gmail.com>
2021-10-03 18:49:53 +0800

    navbar bug fix

--- contribution-app/src/components/About.jsx
+++ contribution-app/src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
     return <section>
         <div className="container px-5 mx-auto flex flex-wrap">
             <br/>
-            <h2 className="p-10 rounded-md text-3xl font-extrabold text-gray-900">About <span style={{ color: "#e05a00" }}>H</span>acktober <span style={{ color: "#e05a00" }}>F</span>est 2021</h2>
+            <h2 style={{ marginTop: 5 + "rem" }} className="p-10 rounded-md text-3xl font-extrabold text-gray-900">About <span style={{ color: "#e05a00" }}>H</span>acktober <span style={{ color: "#e05a00" }}>F</span>est 2021</h2>
             <div className="px-10 rounded-md text-lg text-gray-900">
                 <p class="pb-3">Hacktoberfest is our month-long celebration of the open-source community. During the month of October, we invite you to join open-source software enthusiasts, beginners, and the developer community by contributing to open-source projects. You can do this in a variety of ways:</p>
                 <ul>
