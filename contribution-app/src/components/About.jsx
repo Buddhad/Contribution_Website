e4dbfa30dff68a59ceb8bddb76887922fc59cd81
meixin <meixinchoy@gmail.com>
2021-10-03 13:34:37 +0800

    added about page and content

---
+++ contribution-app/src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+    return <section>
+        <div>
+            <h2>About Hacktober Fest 2021</h2>
+            <div >
+                <div>
+                    <p>Hacktoberfest is our month-long celebration of the open-source community. During the month of October, we invite you to join open-source software enthusiasts, beginners, and the developer community by contributing to open-source projects. You can do this in a variety of ways:</p>
+                    <p>
+                        <ul>
+                            <li>Prepare and share your project for collaboration</li>
+                            <li>Contribute to the betterment of a project via pull request</li>
+                            <li>Organize an event</li>
+                            <li>Mentor others</li>
+                            <li>Donate directly to open source projects</li>
+                        </ul>
+                        <p>For more rules and information regarding this event, take a look at their official website: <a href="http://hacktoberfest.digitalocean.com/">Hacktober Fest 2021</a></p>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+}
+
+export default About;
